0bb8cd0bc8e28afc55df1d84219094301df656d0
AnujChhikara <anujchhikara777@gmail.com>
2023-01-24 21:43:57 +0530

    first commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,103 @@
+function Navbar() {
+  return (
+    <div className="bg-white text-sm pt-8 h-60 text-black font-nunito flex justify-around bg-[url('https://external.fixc10-2.fna.fbcdn.net/emg1/v/t13/1329607189237382277?url=https%3A%2F%2Fwww.datocms-assets.com%2F11645%2F1673349031-tropicals_florals_2023_banner_16x9.png%3Fauto%3Dformat%26fit%3Dmax%26w%3D1200%3Ffit%3Dcrop%26w%3D1080%26h%3D1080&fb_obo=1&utld=datocms-assets.com&stp=c0.5000x0.5000f_dst-jpg_flffffff_p500x261_q75&ccb=13-1&oh=06_AbF4oQFA_FC3n42nkL4Wsw_3xrFkzaDe6S_8dwp0Mn9_QQ&oe=63D1AA49&_nc_sid=aeaded')] ">
+      <div className="flex  justify-center items-center space-x-4 ">
+        <div>SHOP</div>
+        <div>DISCOVER</div>
+        <div>CO-BRAND</div>
+      </div>
+      <div className="flex justify-center items-center">
+        <div>
+          <img
+            className="w-12"
+            src="https://media.glassdoor.com/sql/4197331/chilly-s-bottles-squarelogo-1636379727206.png"
+            alt=""
+          />
+        </div>
+        <div>CHILLY'S</div>
+      </div>
+      <div className="flex justify-center items-center space-x-4">
+        <div>REFER A FRIEND</div>
+        <div>
+          <svg
+            className="w-6 "
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M14 5H20"
+              stroke="#292D32"
+              stroke-width="1.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M14 8H17"
+              stroke="#292D32"
+              stroke-width="1.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M21 11.5C21 16.75 16.75 21 11.5 21C6.25 21 2 16.75 2 11.5C2 6.25 6.25 2 11.5 2"
+              stroke="#292D32"
+              stroke-width="1.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M22 22L20 20"
+              stroke="#292D32"
+              stroke-width="1.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+        </div>
+        <div>
+          <svg
+            className="w-7 fill-transparent hover:fill-pink-200"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              opacity="0.4"
+              d="M7.5 7.67001V6.70001C7.5 4.45001 9.31 2.24001 11.56 2.03001C14.24 1.77001 16.5 3.88001 16.5 6.51001V7.89001"
+              stroke="#292D32"
+              stroke-width="1.5"
+              stroke-miterlimit="10"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M9.0008 22H15.0008C19.0208 22 19.7408 20.39 19.9508 18.43L20.7008 12.43C20.9708 9.99 20.2708 8 16.0008 8H8.0008C3.7308 8 3.0308 9.99 3.3008 12.43L4.0508 18.43C4.2608 20.39 4.9808 22 9.0008 22Z"
+              stroke="#292D32"
+              stroke-width="1.5"
+              stroke-miterlimit="10"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              opacity="0.4"
+              d="M15.4945 12H15.5035"
+              stroke="#292D32"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              opacity="0.4"
+              d="M8.49451 12H8.50349"
+              stroke="#292D32"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Navbar;
