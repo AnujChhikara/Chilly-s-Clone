109744c29680d8d192570e7a7a14eef52734c7e5
Anuj Chhikara <107175639+AnujChhikara@users.noreply.github.com>
2023-02-16 20:24:35 +0530

    Update Carousel.js

--- src/Components/MainBody/Carousel.js
+++ src/Components/MainBody/Carousel.js
@@ -1,12 +1,1 @@
-import AwesomeSlider from "react-awesome-slider";
-import "react-awesome-slider/dist/styles.css";
 
-function Carousel() {
-  const images = ["/main.jpg"];
-  return (
-    <div className="mt-20 px-20">
-      <AwesomeSlider items={images} play={true} interval={3000}></AwesomeSlider>
-    </div>
-  );
-}
-export default Carousel;
